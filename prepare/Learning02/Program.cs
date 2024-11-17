f310e0ef63386a3a6a128c939fc4d5410f763eab
trevin.larsen <soccerbeasttl23@gmail.com>
2024-11-16 21:24:24 -0700

    wo2 pepare

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Create Job instances
+        Job job1 = new Job();
+        job1._jobTitle = "Cloud Architect";
+        job1._company = "Microsoft";
+        job1._startYear = 2017;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "McDonalds";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        // Create Resume instance
+        Resume myResume = new Resume();
+        myResume._name = "Joe John";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        // Display the resume
+        myResume.Display();
     }
 }
